011c858683b79d4983dc2e0a40300c5726c0f77f
MarkusHWeisse <147412030+MarkusHWeisse@users.noreply.github.com>
2024-11-14 10:22:26 +0100

    main.cpp include and int main structure

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "ImageEditor.hpp"
+#include "View/ImageView.hpp"
+#include "Model/ImageModel.hpp"
+#include "Controller/ImageController.hpp"
+
+/*
+
+This will be my personal Photoshop, it will be the best Photoshop clone in the world and in a few years millions will use it.
+
+*/
+
+int main() {
+
+    
+
+    return 0;
+}
